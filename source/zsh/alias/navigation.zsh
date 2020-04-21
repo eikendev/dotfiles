919ceeaecc5fb881d5e84563edd7a7b6bdd5d41f
eikendev <raphael@eiken.dev>
2020-04-21 19:19:30 +0200

    zsh: Adjust command line flags for nnn

--- source/zsh/alias/navigation.zsh
+++ source/zsh/alias/navigation.zsh
@@ -9,4 +9,6 @@ alias gtd='cd ~dotfiles'
 alias gtg='cd ~git'
 
 # Track the working path of nnn.
-alias n='nnn-cd'
+# -e to open text files using $VISUAL
+# -H to show hidden files
+alias n='nnn-cd -e -H'
