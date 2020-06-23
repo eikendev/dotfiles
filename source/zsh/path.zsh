48ecd4e1ace801d8b75d12a28ba171d832273889
eikendev <raphael@eiken.dev>
2020-06-23 12:51:27 +0200

    Prioritize cargo binaries in PATH
    It is important that the cargo-installed binaries are found before
    system-installed ones. Otherwise, we would not be able to switch to any
    toolchain installed by rustup.

--- source/zsh/path.zsh
+++ source/zsh/path.zsh
@@ -1,9 +1,13 @@
+# https://stackoverflow.com/a/18077919
+
 PATH+=:$HOME/.local/bin
 PATH+=:$HOME/bin/dot
-PATH+=:$CARGO_HOME/bin
 PATH+=:$GOPATH/bin
 PATH+=:$DOTFILES/vendor/dasht/bin
-PATH+=:$DOTFILES/vendor/thunderbird-unread
+PATH+=:$DOTFILES/vendor/thunderbird-unread/bin
+
+# Prepend so that rustup works properly.
+path=("$CARGO_HOME/bin" $path)
 
 # Deduplicate entries in $PATH.
 typeset -U PATH
