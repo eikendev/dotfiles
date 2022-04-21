0ad6229216bfc528873e2886a94de1f4c8cdbc52
eikendev <raphael@eiken.dev>
2022-04-21 14:43:57 +0200

    Improve pdf2scan function

--- source/zsh/function/pdf2scan.zsh
+++ source/zsh/function/pdf2scan.zsh
@@ -1,4 +1,5 @@
 # Convert a PDF to a document that appears scanned.
+# https://gist.github.com/andyrbell/25c8632e15d17c83a54602f6acde2724
 function pdf2scan {
 	if [ $# -ne 1 ]; then
 		printf "Usage: $0 <input_file>\n" >&2
@@ -17,7 +18,7 @@ function pdf2scan {
 
 	convert -density 300 "$input_file" -alpha remove -rotate 0.19 -attenuate 0.15 +noise Multiplicative +repage -monochrome -compress group4 -resize 50% "$directory/tmp.jpg"
 
-	convert "$directory/tmp.jpg" -resize 200% -attenuate 0.35 +noise Laplacian -density 300 "$input_file.scan.pdf"
+	convert "$directory/tmp.jpg" -resize 200% -attenuate 0.35 +noise Laplacian -density 300 -sharpen 0x2.5 "$input_file.scan.pdf"
 
 	rm -rf "$directory"
 }
