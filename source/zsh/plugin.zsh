85d34bfc0821aa88eb0d558cd2ed706440a54902
eikendev <raphael@eiken.dev>
2023-02-05 22:46:57 +0100

    Remove forgit

--- source/zsh/plugin.zsh
+++ source/zsh/plugin.zsh
@@ -9,24 +9,6 @@ ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=32
 ZSH_AUTOSUGGEST_USE_ASYNC=1
 
 ################################################################################
-# FORGIT                                                                       #
-################################################################################
-
-FORGIT_FZF_DEFAULT_OPTS="
---height '100%'
-$FZF_DEFAULT_OPTS
-"
-
-forgit_log=ffgl
-forgit_diff=ffgd
-forgit_add=ffga
-forgit_reset_head=ffgch
-forgit_ignore=ffgi
-forgit_restore=ffgcf
-forgit_clean=ffgclean
-forgit_stash_show=ffgs
-
-################################################################################
 # SYNTAX HIGHLIGHTING                                                          #
 ################################################################################
 
