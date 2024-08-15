285d606604f510be3edecd11942a98836b21b326
eikendev <raphael@eiken.dev>
2024-08-15 14:12:46 +0200

    wezterm: Refine configuration

--- source/wezterm/wezterm.lua
+++ source/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 local mux = wezterm.mux
 
 wezterm.on('gui-startup', function(cmd)
@@ -10,7 +11,32 @@ local config = wezterm.config_builder()
 
 config.enable_wayland = true
 
+config.disable_default_key_bindings = true
+config.keys = {
+	{ key="C", mods="CTRL|SHIFT", action=act.CopyTo("ClipboardAndPrimarySelection") },
+	{ key="V", mods="CTRL|SHIFT", action=act.PasteFrom("Clipboard") },
+	{ key = "-", mods = "CTRL", action = act.DecreaseFontSize },
+	{ key = "+", mods = "CTRL", action = act.IncreaseFontSize },
+}
+
+config.window_padding = {
+	left = "8pt",
+	right = "8pt",
+	top = "8pt",
+	bottom = "8pt",
+}
+
+local scheme = wezterm.color.get_builtin_schemes()['MaterialDarker']
+scheme.background = 'black'
+
 config.color_scheme = 'MaterialDarker'
+config.color_schemes = {
+	['MaterialDarker'] = scheme
+}
+
+config.font = wezterm.font 'Monaspace Neon'
+config.font_size = 13
+config.harfbuzz_features = {'calt', 'ss01', 'ss02', 'ss03', 'ss07', 'ss09', 'liga'}
 
 config.window_background_opacity = 0.8
 
@@ -20,6 +46,7 @@ config.check_for_updates = false
 
 config.enable_tab_bar = false
 
-config.font_size = 13
+config.enable_scroll_bar = false
+config.scrollback_lines = 0
 
 return config
