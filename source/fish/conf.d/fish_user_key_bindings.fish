158fa1c65a108da944c3925746b2879c47b5c579
eikendev <raphael@eiken.dev>
2024-08-15 20:31:49 +0200

    fish: Add more key bindings

--- source/fish/conf.d/fish_user_key_bindings.fish
+++ source/fish/conf.d/fish_user_key_bindings.fish
@@ -3,4 +3,6 @@ function fish_user_key_bindings
 	bind -M insert \ce edit_command_buffer
 	bind -M insert \el accept-autosuggestion
 	bind -M insert \e\r 'commandline -f accept-autosuggestion execute'
+	bind -M insert \ej history-search-forward
+	bind -M insert \ek history-search-backward
 end
