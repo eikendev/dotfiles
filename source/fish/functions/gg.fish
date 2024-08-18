2bae615405424d42119e89b7da67cfee8f63fcf8
eikendev <raphael@eiken.dev>
2024-08-18 20:20:46 +0200

    Add wrapper for Yazi

---
+++ source/fish/functions/gg.fish
@@ -0,0 +1,8 @@
+function gg -d 'Change the current working directory when exiting Yazi'
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
