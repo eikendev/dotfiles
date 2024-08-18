50a125d710323d0d603cd06fd48e6282b811ddae
eikendev <raphael@eiken.dev>
2024-08-18 22:03:24 +0200

    fish: Fix genpw functions

--- source/fish/functions/genpw.fish
+++ source/fish/functions/genpw.fish
@@ -1,16 +1,17 @@
-set -l LOWERCASE_CHARS 'abcdefghijklmnopqrstuvwxyz'
-set -l UPPERCASE_CHARS 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-set -l DIGIT_CHARS '1234567890'
-set -l SPECIAL_CHARS ',.-+#!$%&/()=?[]{}*_:;\''
+function genpw -d 'Generate a secure password.'
+	set LOWERCASE_CHARS 'abcdefghijklmnopqrstuvwxyz'
+	set UPPERCASE_CHARS 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	set DIGIT_CHARS '1234567890'
+	set SPECIAL_CHARS ',.-+#!$%&/()=?[]{}*_:;\''
 
-# Secure password generation with special characters.
-function genpw
-    set BASE_CHARS "$LOWERCASE_CHARS$UPPERCASE_CHARS$DIGIT_CHARS$SPECIAL_CHARS"
-    makepasswd -l 32 -n 8 -c "$BASE_CHARS"
-end
+	if test "$argv[1]" = "simple"
+		set BASE_CHARS "$LOWERCASE_CHARS$UPPERCASE_CHARS$DIGIT_CHARS"
+	else if test "$argv[1]" = "complex"
+		set BASE_CHARS "$LOWERCASE_CHARS$UPPERCASE_CHARS$DIGIT_CHARS$SPECIAL_CHARS"
+	else
+		echo "Usage: genpw <simple|complex>"
+		return 1
+	end
 
-# Secure password generation without special characters.
-function genpw-simple
-    set BASE_CHARS "$LOWERCASE_CHARS$UPPERCASE_CHARS$DIGIT_CHARS"
-    makepasswd -l 32 -n 8 -c "$BASE_CHARS"
+	makepasswd -l 32 -n 8 -c "$BASE_CHARS"
 end
